39041364b9ed4b32ce61fd2f7145067af06b439e
Revan Putra Wahyu <revanputra3768@gmail.com>
2025-09-28 15:43:17 +0700

    tambah php

---
+++ p2/b5.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <title>
+      Data Lengkap
+    </title>
+  </head>
+  <body>
+    <?php
+    $nama = "Lintang Seruni Senja";
+    $alamat = "Jl. Badak No. 2 Cimahi-Bandung";
+    define("TTL", "Jakarta, 20 Oktober 1990");
+    define("Telp", "0812345678");
+
+    echo "Nama Saya : $nama <br>";
+    echo "Alamat : $alamat <br>";
+    echo TTL . "<br>";
+    echo Telp;
+    ?>
+  </body>
+</html>
