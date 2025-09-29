0f2ba982c515ed53978a0495134b22d69b9ea74c
Revan Putra Wahyu <revanputra3768@gmail.com>
2025-09-29 09:34:59 +0700

    nambah lagi

---
+++ p3/b4.php
@@ -0,0 +1,7 @@
+<?php
+$teks1 = "Aku Sedang Belajar ";
+$teks2 = "Pemograman WEB ";
+$teks3 = "Menggunakan Bahasa Pemograman PHP";
+$hasil = $teks1 . $teks2 . $teks3;
+echo $hasil;
+?>
---
+++ p3/b5.php
@@ -0,0 +1,10 @@
+<?php
+$panjang = 15;
+$volumeBalok = $panjang*$panjang*$panjang;
+$teks1 = "Belajar Menghitung . ";
+$teks2 = "Volume Kubus";
+
+echo $teks1 . $teks2 . "<br>";
+echo "Panjang Sisi Kubus = $panjang <br>";
+echo "Volume Kubus = $volumeBalok cm3";
+?>
