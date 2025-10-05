4b8260edb4d783cf9074e93205713f01c8f398ba
Revan Putra Wahyu <revanputra3768@gmail.com>
2025-10-05 16:29:00 +0700

    baru

---
+++ p7/b3.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>Penggunaan WHILE</title>
+  </head>
+  <body>
+    <br>
+    <?php
+    $jumlah=1;
+    while ($jumlah<=5){
+      echo $jumlah++;
+      echo "<br>";
+    }
+    ?>
+  </body>
+</html>
---
+++ p7/b4.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>Penggunaan Do WHILE</title>
+  </head>
+  <body>
+    Menggunakan Do WHILE
+    <br>
+    <?php
+    $jumlah= 10;
+    do{
+      echo $jumlah++;
+      echo "<br>";
+    }
+    while ($jumlah <=1)
+    ?>
+  </body>
+</html>
